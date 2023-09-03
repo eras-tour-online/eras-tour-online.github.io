c4a2f83e20006c23b3ac64e17187b8b5a7e33bf3
eras-tour-online <143968418+eras-tour-online@users.noreply.github.com>
2023-09-04 00:42:14 +0300

    Add files via upload

---
+++ registration.js
@@ -0,0 +1,12 @@
+function regsucc() {
+    document.getElementById("registration-page").style.display="none";
+              document.getElementById("registration-successful").style.display="block";
+              document.getElementById("registration-unsuccessful").style.display="none";
+              window.scrollTo(0,0);
+}
+function regunsucc() {
+    document.getElementById("registration-page").style.display="none";
+              document.getElementById("registration-successful").style.display="none";
+              document.getElementById("registration-unsuccessful").style.display="block";
+              window.scrollTo(0,0);
+}
